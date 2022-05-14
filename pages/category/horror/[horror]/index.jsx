b8ca0eedd4ca7414b44bd0783f016c11e2443be2
rafael-mingossi <rafaelmingossi@gmail.com>
2022-05-14 14:52:03 +1000

    test

--- pages/category/horror/[horror]/index.jsx
+++ pages/category/horror/[horror]/index.jsx
@@ -1,55 +1,56 @@
 import SingleBookPage from '../../../../src/components/SingleBookPage';
 
-const Horror = ({ horror }) => {
-  return <SingleBookPage book={horror[0]} />;
+const Horror = () => {
+  // return <SingleBookPage book={horror[0]} />;
+  return <div>hey</div>;
 };
 
 export default Horror;
 
-export async function getStaticPaths() {
-  const env = process.env.NODE_ENV;
-  let url = '';
+// export async function getStaticPaths() {
+//   const env = process.env.NODE_ENV;
+//   let url = '';
 
-  if (env === 'development') {
-    url = process.env.DEV;
-  } else if (env === 'production') {
-    url = process.env.PROD;
-  }
+//   if (env === 'development') {
+//     url = process.env.DEV;
+//   } else if (env === 'production') {
+//     url = process.env.PROD;
+//   }
 
-  const hor = await fetch(url);
-  const horr = await hor?.json();
+//   const hor = await fetch(url);
+//   const horr = await hor?.json();
 
-  const paths = horr?.books?.map((num) => ({
-    params: { horror: num.bookId },
-  }));
+//   const paths = horr?.books?.map((num) => ({
+//     params: { horror: num.bookId },
+//   }));
 
-  return { paths, fallback: true };
-}
+//   return { paths, fallback: true };
+// }
 
-export async function getStaticProps({ params }) {
-  const env = process.env.NODE_ENV;
-  let url = '';
+// export async function getStaticProps({ params }) {
+//   const env = process.env.NODE_ENV;
+//   let url = '';
 
-  if (env === 'development') {
-    url = process.env.DEV;
-  } else if (env === 'production') {
-    url = process.env.PROD;
-  }
+//   if (env === 'development') {
+//     url = process.env.DEV;
+//   } else if (env === 'production') {
+//     url = process.env.PROD;
+//   }
 
-  const hor = await fetch(url);
-  const horr = await hor?.json();
+//   const hor = await fetch(url);
+//   const horr = await hor?.json();
 
-  const horror = horr?.books
-    ?.map((book) => ({
-      bookId: book?.bookId,
-      cover: book?.cover,
-      title: book?.title,
-      category: book?.category,
-      description: book?.description,
-      totalPages: book?.totalPages,
-      year: book?.year,
-    }))
-    .filter(({ bookId }) => bookId === params?.horror);
+//   const horror = horr?.books
+//     ?.map((book) => ({
+//       bookId: book?.bookId,
+//       cover: book?.cover,
+//       title: book?.title,
+//       category: book?.category,
+//       description: book?.description,
+//       totalPages: book?.totalPages,
+//       year: book?.year,
+//     }))
+//     .filter(({ bookId }) => bookId === params?.horror);
 
-  return { props: { horror }, revalidate: 600 };
-}
+//   return { props: { horror }, revalidate: 600 };
+// }
--- pages/category/suspense/[suspense]/index.jsx
+++ pages/category/suspense/[suspense]/index.jsx
@@ -1,54 +1,55 @@
 import SingleBookPage from '../../../../src/components/SingleBookPage';
 
-const Suspense = ({ suspense }) => {
-  return <SingleBookPage book={suspense[0]} />;
+const Suspense = () => {
+  //return <SingleBookPage book={suspense[0]} />;
+  return <div>hey</div>;
 };
 
 export default Suspense;
 
-export async function getStaticPaths() {
-  const env = process.env.NODE_ENV;
-  let url = '';
+// export async function getStaticPaths() {
+//   const env = process.env.NODE_ENV;
+//   let url = '';
 
-  if (env === 'development') {
-    url = process.env.DEV;
-  } else if (env === 'production') {
-    url = process.env.PROD;
-  }
+//   if (env === 'development') {
+//     url = process.env.DEV;
+//   } else if (env === 'production') {
+//     url = process.env.PROD;
+//   }
 
-  const sus = await fetch(url);
-  const susp = await sus.json();
+//   const sus = await fetch(url);
+//   const susp = await sus.json();
 
-  const paths = susp?.books?.map((num) => ({
-    params: { suspense: num.bookId },
-  }));
+//   const paths = susp?.books?.map((num) => ({
+//     params: { suspense: num.bookId },
+//   }));
 
-  return { paths, fallback: true };
-}
+//   return { paths, fallback: true };
+// }
 
-export async function getStaticProps({ params }) {
-  const env = process.env.NODE_ENV;
-  let url = '';
+// export async function getStaticProps({ params }) {
+//   const env = process.env.NODE_ENV;
+//   let url = '';
 
-  if (env === 'development') {
-    url = process.env.DEV;
-  } else if (env === 'production') {
-    url = process.env.PROD;
-  }
-  const sus = await fetch(url);
-  const susp = await sus.json();
+//   if (env === 'development') {
+//     url = process.env.DEV;
+//   } else if (env === 'production') {
+//     url = process.env.PROD;
+//   }
+//   const sus = await fetch(url);
+//   const susp = await sus.json();
 
-  const suspense = susp?.books
-    ?.map((book) => ({
-      bookId: book?.bookId,
-      cover: book?.cover,
-      title: book?.title,
-      category: book?.category,
-      description: book?.description,
-      totalPages: book?.totalPages,
-      year: book?.year,
-    }))
-    .filter(({ bookId }) => bookId === params.suspense);
+//   const suspense = susp?.books
+//     ?.map((book) => ({
+//       bookId: book?.bookId,
+//       cover: book?.cover,
+//       title: book?.title,
+//       category: book?.category,
+//       description: book?.description,
+//       totalPages: book?.totalPages,
+//       year: book?.year,
+//     }))
+//     .filter(({ bookId }) => bookId === params.suspense);
 
-  return { props: { suspense }, revalidate: 600 };
-}
+//   return { props: { suspense }, revalidate: 600 };
+// }
